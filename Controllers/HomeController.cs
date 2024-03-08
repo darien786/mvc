b61670cc78b46de1b8ed44c4b6a3a7ca0cf7ba70
Darien Baltazar <darienbaltazar2@gmail.com>
2024-03-08 13:02:45 -0600

    aplicacion en c#

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvc;
+
+public class HomeController : Controller{
+
+    private readonly IDataContext _context;
+
+    public HomeController(IDataContext context){
+        _context = context;
+    }
+
+    public async Task<IActionResult> index(){
+        List<Producto> productos = await _context.ObtenProductosAsync();
+
+        return View(productos);
+    }
+
+    public IActionResult Error(){
+        return View();
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,10 @@
+namespace mvc;
+
+public class Producto{
+
+    public int ProductoId { get; set; }
+    public String? Nombre { get; set; }
+    public decimal Precio { get; set; }
+    public String? Fabricante { get; set; }
+    
+}
